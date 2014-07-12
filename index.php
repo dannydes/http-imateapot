92f0835e725e8861bcc55451e7d6200554d99862
Daniel <desiradaniel2007@hotmail.com>
2014-07-12 12:19:07 +0200

    Greenish background

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
 header('HTTP/1.1 418 I\'m a teapot');
+
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8" />
+
+		<link href="style.css" rel="stylesheet"></link>
+
 		<title>I'm a teapot</title>
 	</head>
 	<body>
